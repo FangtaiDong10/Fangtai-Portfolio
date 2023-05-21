e388af899530ff405bf8c0c7593921b8c32bc830
Fangtai Dong <106503583+FangtaiDong10@users.noreply.github.com>
2023-05-22 00:28:44 +1000

    app layout construction
    App.js layout construction and
    fortawesome icon packages installation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { AlertProvider } from "./context/alertContext";
+import Alert from "./components/Alert";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 
 function App() {
-  
+  return (
+    <ChakraProvider>
+      <AlertProvider>
+        <main>
+          <Header />
+          <Footer />
+          <Alert />
+        </main>
+      </AlertProvider>
+    </ChakraProvider>
+  )
 }
 
 export default App;
